af936f856e5615798ae380069fb20440e6955775
umphmeow <131490186+umphmeow@users.noreply.github.com>
2023-04-22 02:10:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 """
 dict = {'1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять', '0': 'ноль', '.': 'целых', ',': 'десятых'}
 
-
 with open('text.txt', 'r') as f_in:
     block_size = 100
     d_array = []
     data = f_in.read(block_size)
     while data:
+        #проверка и создание массива чисел
         digits = [d.replace('77', '', 1) for d in data.split() if d.isdigit() and d.startswith('77') and int(d) != '77']
         d_array.extend(digits)
         while '' in d_array:
