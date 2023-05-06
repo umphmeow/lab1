7b46d1c857ee15f39bdf9bde7f34e466d31cb003
Ilya K <131490186+umphmeow@users.noreply.github.com>
2023-05-06 10:13:14 +0400

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,16 @@ with open('text.txt', 'r') as f_in:
         data = f_in.read(block_size)
 
 if not d_array:
-    print('Не найдено цифр удовлетворяющих заданию или цифр нет')
+    print('Не найдено цифр удовлетворяющих заданию или цифр нет вообще')
 else:
+    print('Массив чисел:', d_arr_int)
     max_d = max(d_arr_int)
     min_d = min(d_arr_int)
+    print('Максимальное число:', max_d)
+    print('Минимальное число:', min_d)
     answer = (max_d + min_d) / 2
-       answer_txt = ''
+    print('Среднее арифметическое:', answer)
+    answer_txt = ''
     for digit in str(answer):
-            answer_txt += dict[digit] + ' '
-    print('Ответ:', answer_txt, dict[','] )
+        answer_txt += ' ' + dict[digit]
+    print('Ответ:', answer_txt, dict[','])
